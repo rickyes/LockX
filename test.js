fdf959f2928023fb06991df56d2c41439ab14a8f
zhoumingque <mail@zhoumq.cn>
2018-02-04 13:40:08 +0800

    初始化项目

---
+++ test.js
@@ -0,0 +1,15 @@
+const Lock = require('./index');
+const lockName = 'test';
+console.log(Lock);
+let index = 10;
+while (index-- > 0) {
+  Lock.lock(lockName, function(data) {
+    console.log('lock', data);
+    setTimeout(() => {
+      console.log('unlock');
+      Lock.releaseLock(lockName);
+    }, 1000);
+  }, {
+    index
+  });
+}
